e1b3b336e6dda9215c3e28fdb916561f6bbae7ca
kenchasonakai <kenchasonakai@gmail.com>
2025-07-06 05:10:50 +0000

    add: ExamSession用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    resources :exam_sessions, only: [ :index, :show ], param: :slug
+  end
 end
